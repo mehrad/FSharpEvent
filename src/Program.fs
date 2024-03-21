6fb11b8fbd58d283246afa883abbfb81abfa8bf9
Mahrad <hossein.ataeefard@gmail.com>
2024-03-21 19:13:49 +0000

    More functional

--- src/Program.fs
+++ src/Program.fs
@@ -1,42 +1,45 @@
 ﻿open System
 open NonNegativeDuration
 open ScheduledEvent
+open System.IO
 
-let mutable events = System.Collections.Generic.Dictionary<Guid, ScheduledEvent>() 
+let initialEvents = Seq.empty<ScheduledEvent>
 
-let addEvent (event: ScheduledEvent) =
-    events.Add(event.Id, event)
+let addEvent event eventsSeq =
+    seq {
+        yield! eventsSeq  // Yield all events currently in the sequence
+        yield event       // Add the new event to the sequence
+    }
 
-let viewEvents () =
-    for event in events.Values do
-        printfn "Event: %s" event.Title
+let viewEvents eventsSeq =
+    eventsSeq |> Seq.iter (fun e -> 
+        printfn "Id: %A, Title: %s, Start Date: %O, Duration: %d minutes" e.Id e.Title e.StartDate (NonNegativeDuration.value e.Duration))
 
-let deleteEvent (eventId: Guid) =
-    events.Remove(eventId)
+let deleteEvent eventId eventsSeq =
+    eventsSeq |> Seq.filter (fun e -> e.Id <> eventId)
 
 let parseEvent (line: string) : Option<ScheduledEvent> =
     let parts = line.Split(',')
-    if parts.Length <> 4 then None
-    else
+    if parts.Length = 4 then
         let title = parts.[0]
         let description = parts.[1]
         let startDate = DateTime.Parse(parts.[2])
-        let duration = NonNegativeDuration.create (int parts.[3]) |> Option.get
-        Some(ScheduledEvent.create title description startDate duration)
+        let duration = NonNegativeDuration.create (int parts.[3])
+        match duration with
+        | Some d -> Some (ScheduledEvent.create title description startDate d)
+        | None -> None
+    else
+        None
 
 
-let createEventsFromFile (filePath: string) : ScheduledEvent list =
-    let lines = System.IO.File.ReadAllLines(filePath)
-    lines |> Array.toList |> List.map parseEvent |> List.choose id
-
-
+let createEventsFromFile (filePath: string) : seq<ScheduledEvent> =
+    File.ReadLines(filePath)
+    |> Seq.map parseEvent
+    |> Seq.choose id
 
 [<EntryPoint>]
 let main argv =
-    let duration = NonNegativeDuration.create 90 |> Option.get
-    createEventsFromFile "events.txt" |> List.iter addEvent
+    createEventsFromFile "events.txt"
+    |> viewEvents
 
-
-    viewEvents()
-
-    0 // Return an integer exit code
+    0 // return an integer exit code
